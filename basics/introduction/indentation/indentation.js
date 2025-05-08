4f90d81d1a373e4c08f34e20f32f32707cbef88e
Vladimir Elsukov <groupdirect@yandex.ru>
2025-05-08 15:22:33 +0300

    Добавление блоков кода в папку introduction

---
+++ basics/introduction/indentation/indentation.js
@@ -0,0 +1,10 @@
+// Хорошее форматирование с отступами
+function myFunction() {
+    let message = "Привет";
+    if (message === "Привет") {
+        console.log(message);
+    }
+}
+
+myFunction(); // Результат: Привет
+
